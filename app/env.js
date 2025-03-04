e28fa8e28c583764824fbc42eda6fc8792a576e1
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-03-04 16:57:15 -0700

    Added variables to enj.js from lab page

--- app/env.js
+++ app/env.js
@@ -2,7 +2,11 @@
 
 export const dev = window.location.origin.includes('localhost')
 // NOTE api instance of axios uses this exported variable as its baseUrl
-export const baseURL = 'https://sandbox.codeworksacademy.com/'
+export const baseURL = dev ? 'https://sandbox.codeworksacademy.com' : ''
+export const useSockets = false
 export const domain = 'codeworksclassroom.auth0.com'
-export const audience = 'https://codeworksclassroom.com'
 export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+export const audience = 'https://codeworksclassroom.com'
+
+
+
